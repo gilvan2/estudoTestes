43b842b6fff88d29ecbd1b68249e9de5547f2769
Gilvan2 <gilvan.silva.junior@gmail.com>
2024-03-24 06:45:55 -0300

    primeiro test funcional acessando uma página e fazendo acertivas

---
+++ cypress/01-basico/cypress/e2e/3-cypress-basico/basis.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+describe('Basico do cypress',()=>{
+    it('Visitando uma página e fazer uma acertiva no titulo',()=>{
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+        cy.title().should('be.equal','Campo de Treinamento')
+        cy.title().should('contains','Campo')
+
+        cy.title()
+            .should('be.equal','Campo de Treinamento')
+            .and('contain','Campo')
+
+            //TODO imprimir o log no console
+            //TODO escrever o site em um campo texto
+    })
+})
