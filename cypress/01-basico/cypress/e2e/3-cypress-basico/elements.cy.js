38e59bed9ff13833de4fb68b1b04e4d448c92297
Gilvan2 <gilvan.silva.junior@gmail.com>
2024-03-27 23:34:06 -0300

    trabalhando o básico de links

--- cypress/01-basico/cypress/e2e/3-cypress-basico/elements.cy.js
+++ cypress/01-basico/cypress/e2e/3-cypress-basico/elements.cy.js
@@ -3,7 +3,21 @@
 describe('Trabalhando com elementos básicos',()=>{
     it('Textos',()=>{
         cy.visit('https://wcaquino.me/cypress/componentes.html')
-        cy.get('span').should('contain','Cuidado onde clica, ')//Uma tag
+        cy.get('span').should('contain','Cuidado onde clica, ')//Uma classe css(tag)
         cy.get('.facilAchar').should('have.text','Cuidado onde clica, muitas armadilhas...')//Uma classe -> busca exatamente o elemento
+    })
+
+    it('Links',()=>{
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.get('[href="#"]').click()
+        cy.get('#resultado').should('have.text','Voltou!')
+
+        cy.reload()
+        cy.get('#resultado').should('have.not.text','Voltou!')
+        cy.contains('Voltar').click()
+        cy.get('#resultado').should('have.text','Voltou!')
+
+
+
     })
 })
