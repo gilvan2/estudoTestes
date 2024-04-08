2713f0244ea35e6a392f71b82eb0257fbddd9299
Gilvan2 <gilvan.silva.junior@gmail.com>
2024-04-07 23:54:41 -0300

    Interação com campso do tipo text fild

--- cypress/01-basico/cypress/e2e/3-cypress-basico/elements.cy.js
+++ cypress/01-basico/cypress/e2e/3-cypress-basico/elements.cy.js
@@ -21,4 +21,25 @@ describe('Trabalhando com elementos básicos',()=>{
         cy.get('#resultado').should('have.text','Voltou!')
 
     })
+
+    it('TextFild',()=>{
+        cy.get('#formNome').type('Cypress Test')
+        cy.get('#formNome').should('have.value','Cypress Test')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .type('TextArea')//No jQery Selector, os : está confundindo o Cypress, por isso a adição do caracter de escape
+            .should('have.value','TextArea')
+
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+            .type('Campo1')
+
+        cy.get('[data-cy="dataSobrenome"]')
+            .type('Teste12345{backspace}{backspace}')
+            .should('have.value','Teste123')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .clear()
+            .type('Erro{selectall}acerto',{delay:100})
+            .should('have.value','acerto')
+    })
 })
