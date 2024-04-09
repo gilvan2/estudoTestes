f04cb88ca5debdf449de933ad76cdb212381e8e6
Gilvan2 <gilvan.silva.junior@gmail.com>
2024-04-09 14:07:14 -0300

    estudo usando wait e timeout

--- cypress/01-basico/cypress/e2e/3-cypress-basico/sync.cy.js
+++ cypress/01-basico/cypress/e2e/3-cypress-basico/sync.cy.js
@@ -12,7 +12,7 @@ describe('Esperas e sincronizações',()=>{
         cy.get('#novoCampo').type('funciona')
     })
 
-    it.only('Usando o find para buscas compostas',()=>{
+    it('Usando o find para buscas compostas',()=>{
 
         cy.get('#buttonList').click()
         cy.get('#lista li')
@@ -24,6 +24,20 @@ describe('Esperas e sincronizações',()=>{
 
             cy.get('#lista li span')//Caso o cypress se perca no contexto, deixar o find e usar a chamada completa do elemento
             .should('contain','Item 2')
+    })
+
+    it.only('Uso do Timeout',()=>{
+
+        cy.get('#buttonListDOM').click()
+        cy.wait(5000)//O script sempre irá esperar o tempo informado
+        cy.get('#lista li span')
+            .should('contain','Item 2')
+
+        cy.get('#buttonListDOM').click()
+        cy.get('#lista li span',{timeout: 3000})//Usando o timeout, vc seta o máximo que pode esperar para dar o erro, mas avança se a condição for satisfeita
+            .should('contain','Item 2')
+        
+
     })
 
     
