98ec561ff5ebd7dd8f2d65eb4e451cd59ea375e5
Gilvan2 <gilvan.silva.junior@gmail.com>
2024-04-14 01:24:33 -0300

    navegando nos campos dos objetos fornecidos ao cypress pelo its

--- cypress/01-basico/cypress/e2e/3-cypress-basico/helpers.cy.js
+++ cypress/01-basico/cypress/e2e/3-cypress-basico/helpers.cy.js
@@ -4,7 +4,7 @@ describe('Helpers...',()=>{
         cy.visit('https://wcaquino.me/cypress/componentes.html')
     })*/
     it('Wrap',()=>{
-        const obj = {nome: 'user', idade: 20}
+        const obj = {nome: 'User', idade: 20}
         expect(obj).to.have.property('nome')
         cy.wrap(obj).should('have.property','nome')
         /*Case estejamos usando um objeto, o cypress não sabe usar shoud
@@ -40,4 +40,22 @@ describe('Helpers...',()=>{
 
         })
     })
+
+    it('Its...',()=>{
+        const obj = {nome: 'User', idade: 20}
+        cy.wrap(obj).should('have.property','nome','User')
+        cy.wrap(obj).its('nome').should('be.equal','User') 
+        //O itens busca uma propriedade especifica do objeto fornecido
+
+        const obj2 = {nome: 'User', idade: 20, endereco: {rua:'dos bobos'}}
+        cy.wrap(obj2).its('endereco').should('have.property', 'rua')
+        cy.wrap(obj2).its('endereco').its('rua').should('contain', 'bobos')
+        cy.wrap(obj2).its('endereco.rua').should('contain','bobos')
+        //Tudo faz basicamente a mesma coisa no código acima
+
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.title().its('length').should('be.equal', 20)      
+    })
+
+
 })
