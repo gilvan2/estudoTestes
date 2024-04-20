33016aa5ac362c5cdb217850d1e65d42162a0e4c
Gilvan2 <gilvan.silva.junior@gmail.com>
2024-04-20 01:06:46 -0300

    alert com stub

--- cypress/01-basico/cypress/e2e/3-cypress-basico/alert.cy.js
+++ cypress/01-basico/cypress/e2e/3-cypress-basico/alert.cy.js
@@ -16,4 +16,13 @@ describe('Trabalhando com alert', ()=>{
             expect(msg).to.be.equal('Alert Simples')
         })
     })
+
+    //TO DO alert com stub não funciona, mesmo colando a resposta do repositório
+    /*it('Alert com mock', () => {
+        const stub = cy.stub().as('alerta')
+        cy.on('window:alert', stub)
+        cy.get('#alert').click().then(() => {
+            expect(stub.getCall(0)).to.be.calledWith('Alert Simples')
+        })
+    })      */ 
 })
