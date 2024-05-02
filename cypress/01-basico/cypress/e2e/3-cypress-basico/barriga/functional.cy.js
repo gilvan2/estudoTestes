3878339e7fadedeb44904d2ba56fc5113b9dc7f5
Gilvan2 <gilvan.silva.junior@gmail.com>
2024-05-02 00:54:50 -0300

    criação do cenário que edita uma conta criada

--- cypress/01-basico/cypress/e2e/3-cypress-basico/barriga/functional.cy.js
+++ cypress/01-basico/cypress/e2e/3-cypress-basico/barriga/functional.cy.js
@@ -23,4 +23,20 @@ describe('Testanado em nível funcional',()=>{
         cy.get('.toast').should('contain','Conta inserida com sucesso')
     })
 
+    it.only('Deve alterar uma conta', ()=>{
+        cy.get('[data-test=menu-settings]').click()
+        cy.get('[href="/contas"]').click()
+
+        cy.contains('tr', 'Conta de testes') // Localize a linha que contém o texto "Conta de testes"
+        .find('i.far.fa-edit') // Encontre o elemento <i> com as classes "far" e "fa-edit" dentro da linha
+        .click(); // Clique no botão "far fa-edit"
+
+        cy.get('[data-test="nome"]').clear().type('Conta de testes alterada')
+
+        cy.get('button.btn').click()
+        cy.get('.toast').should('contain','Conta atualizada com sucesso')
+
+
+    })
+
 })
