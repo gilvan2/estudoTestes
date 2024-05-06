675b3d0a2fe73a3049b52b3835b065005fb8e343
Gilvan2 <gilvan.silva.junior@gmail.com>
2024-05-06 11:33:16 -0300

    refatoração para separação dos comandos especificos de login

--- cypress/01-basico/cypress/e2e/3-cypress-basico/barriga/functional.cy.js
+++ cypress/01-basico/cypress/e2e/3-cypress-basico/barriga/functional.cy.js
@@ -1,40 +1,32 @@
 /// <reference types="cypress" />
 
 import loc from '../../../support/locators'
+import '../../../support/commandsContas'
 
 describe('Testanado em nível funcional',()=>{
     beforeEach(()=>{
-
         cy.login('gilvan.silva.junior@gmail.com','Fisi123c@')
-
     })
 
     it('Deve cadastrar uma conta',()=>{
 
         cy.resetApp()
-
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
-        cy.get(loc.CONTA.NOME).type('Conta de testes')
-        cy.get(loc.CONTA.BTN_SALVAR).click()
+        cy.acessarMenuConta()
+        cy.inserirConta('Conta de testes')
         cy.get(loc.MESSAGE).should('contain','Conta inserida com sucesso')
     })
 
     it('Deve alterar uma conta', ()=>{
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
-
-        cy.contains('tr', 'Conta de testes') // Localize a linha que contém o texto "Conta de testes"
-        .find(loc.CONTA.LOCATOR_BTN_ALTERAR)//.find('i.far.fa-edit') // Encontre o elemento <i> com as classes "far" e "fa-edit" dentro da linha
-        .click(); // Clique no botão "far fa-edit"
+        
+        cy.acessarMenuConta()
+        cy.contains('tr', 'Conta de testes')
+        .find(loc.CONTA.LOCATOR_BTN_ALTERAR)
+        .click();
 
         cy.get(loc.CONTA.NOME).clear().type('Conta de testes alterada')
 
-        //cy.get('button.btn').click()
         cy.get(loc.CONTA.BTN_SALVAR).click()
         cy.get(loc.MESSAGE).should('contain','Conta atualizada com sucesso')
 
     })
-    
-
 })
---
+++ cypress/01-basico/cypress/support/commandsContas.js
@@ -0,0 +1,14 @@
+import loc from './locators'
+
+Cypress.Commands.add('acessarMenuConta', ()=>{
+    cy.get(loc.MENU.SETTINGS).click()
+cy.get(loc.MENU.CONTAS).click()
+})
+
+Cypress.Commands.add('inserirConta',(conta)=>{
+    cy.get(loc.CONTA.NOME).type(conta)
+cy.get(loc.CONTA.BTN_SALVAR).click()
+
+})
+
+
