e71c5a454de9fe82d05df2d377d96973614f0013
Gilvan2 <gilvan.silva.junior@gmail.com>
2024-05-09 23:23:15 -0300

    teste para validar inserção de movimentação padrão

--- cypress/01-basico/cypress/support/locators.js
+++ cypress/01-basico/cypress/support/locators.js
@@ -7,13 +7,20 @@ const locators = {
     MENU: {
         SETTINGS: '[data-test=menu-settings]',
         CONTAS: '[href="/contas"]',
-        RESETAR: '[href="/reset"]'
+        RESETAR: '[href="/reset"]',
+        MOVIMENTACAO: '[data-test="menu-movimentacao"]'
     },
     CONTA: {
         NOME: '[data-test="nome"]',
         BTN_SALVAR: 'button.btn',
         //LOCALIZAR_BTN_ALTERAR:"'tr', 'Conta de testes'",
         LOCATOR_BTN_ALTERAR: 'i.far.fa-edit'
+    },
+    MOVIMENTACAO :{
+        DESCRICAO: '[data-test="descricao"]',
+        VALOR: '[data-test="valor"]',
+        INTERESSADO: '[data-test="envolvido"]',
+        BTN_SALVAR: '.btn-primary'
     },
     MESSAGE: '.toast'
 
