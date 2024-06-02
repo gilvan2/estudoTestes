6709fc58c1f64653dc0b7a0dce18205f6d3fb36a
Gilvan2 <gilvan.silva.junior@gmail.com>
2024-06-02 05:10:55 -0300

    mock para insersão de transações e validação de nº de registros no extrato

--- cypress/01-basico/cypress/e2e/3-cypress-basico/barriga/frontend.cy.js
+++ cypress/01-basico/cypress/e2e/3-cypress-basico/barriga/frontend.cy.js
@@ -57,7 +57,7 @@ describe('Testanado em nível funcional',()=>{
 
     })
 
-    it.only('não deve inserir conta com nome repetido',()=>{
+    it('não deve inserir conta com nome repetido',()=>{
         //cy.acessarMenuConta()
         cy.intercept({
             method: 'POST',
@@ -73,9 +73,25 @@ describe('Testanado em nível funcional',()=>{
         cy.get(loc.MESSAGE).should('contain','code 400')
     })
 
-    it('Deve cadastrar uma movimentação', ()=>{
+    it.only('Deve cadastrar uma movimentação', ()=>{
 
         let movimentacoesNoExtrato;
+//Registros para leitura do extrato
+//-------------------------------------------------------------------------------------
+        cy.intercept({
+            method: 'GET',
+            url: 'https://barrigarest.wcaquino.me/extrato/**'
+        },[
+            { conta: "Conta para saldo", id: 2011776,descricao: "Movimentacao 1, calculo saldo", envolvido: "CCC", observacao: null ,tipo: "REC", data_transacao: "2024-05-31T03:00:00.000Z", data_pagamento: "2024-05-31T03:00:00.000Z", valor: "3500.00", status: true, conta_id: 2145231, usuario_id: 99999, transferencia_id: null, parcelamento_id: null},
+            { conta: "Conta com movimentacao", id: 2011775,descricao: "Movimentacao de conta", envolvido: "BBB", observacao: null ,tipo: "DESP", data_transacao: "2024-05-31T03:00:00.000Z", data_pagamento: "2024-05-31T03:00:00.000Z", valor: "-1500.00", status: true, conta_id: 2145230, usuario_id: 99999, transferencia_id: null, parcelamento_id: null},
+            { conta: "Conta para saldo", id: 2011777,descricao: "Movimentacao 2, calculo saldo", envolvido: "DDD", observacao: null ,tipo: "DESP", data_transacao: "2024-05-31T03:00:00.000Z", data_pagamento: "2024-05-31T03:00:00.000Z", valor: "-1000.00", status: true, conta_id: 2145231, usuario_id: 99999, transferencia_id: null, parcelamento_id: null},
+            { conta: "Conta para saldo", id: 2011778,descricao: "Movimentacao 3, calculo saldo", envolvido: "EEE", observacao: null ,tipo: "REC", data_transacao: "2024-05-31T03:00:00.000Z", data_pagamento: "2024-05-31T03:00:00.000Z", valor: "1534.00", status: true, conta_id: 2145231, usuario_id: 99999, transferencia_id: null, parcelamento_id: null},
+            { conta: "Conta para extrato", id: 2011779,descricao: "Movimentacao para extrato", envolvido: "FFF", observacao: null ,tipo: "DESP", data_transacao: "2024-05-31T03:00:00.000Z", data_pagamento: "2024-05-31T03:00:00.000Z", valor: "-220.00", status: true, conta_id: 2145232, usuario_id: 99999, transferencia_id: null, parcelamento_id: null},
+            { conta: "Conta para movimentacoes", id: 2011780, descricao: "desc", envolvido: "inter", observacao: null, tipo: "REC" , data_transacao: "2024-05-31T03:00:00.000Z", data_pagamento: "2024-06-01T03:00:00.000Z", valor: "123.00", status: true, conta_id: 2145229, usuario_id: 99999, transferencia_id: null, parcelamento_id: null }
+        ]
+        ).as('estrato_com_6_registros')
+//-------------------------------------------------------------------------------------
+
         cy.get(loc.MENU.EXTRATO).click()
         cy.get('.list-group')
         .find('li')
@@ -90,6 +106,24 @@ describe('Testanado em nível funcional',()=>{
         cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
         cy.get(loc.MESSAGE).should('contain','Movimentação inserida com sucesso!')
         //Garante que foi adicionada uma movimentação desde o inicio do processo
+//Registros para leitura do extrato        
+//------------------------------------------------------------------------------------        
+cy.intercept({
+    method: 'GET',
+    url: 'https://barrigarest.wcaquino.me/extrato/**'
+},[
+    { conta: "Conta para saldo", id: 2011776,descricao: "Movimentacao 1, calculo saldo", envolvido: "CCC", observacao: null ,tipo: "REC", data_transacao: "2024-05-31T03:00:00.000Z", data_pagamento: "2024-05-31T03:00:00.000Z", valor: "3500.00", status: true, conta_id: 2145231, usuario_id: 99999, transferencia_id: null, parcelamento_id: null},
+    { conta: "Conta com movimentacao", id: 2011775,descricao: "Movimentacao de conta", envolvido: "BBB", observacao: null ,tipo: "DESP", data_transacao: "2024-05-31T03:00:00.000Z", data_pagamento: "2024-05-31T03:00:00.000Z", valor: "-1500.00", status: true, conta_id: 2145230, usuario_id: 99999, transferencia_id: null, parcelamento_id: null},
+    { conta: "Conta para saldo", id: 2011777,descricao: "Movimentacao 2, calculo saldo", envolvido: "DDD", observacao: null ,tipo: "DESP", data_transacao: "2024-05-31T03:00:00.000Z", data_pagamento: "2024-05-31T03:00:00.000Z", valor: "-1000.00", status: true, conta_id: 2145231, usuario_id: 99999, transferencia_id: null, parcelamento_id: null},
+    { conta: "Conta para saldo", id: 2011778,descricao: "Movimentacao 3, calculo saldo", envolvido: "EEE", observacao: null ,tipo: "REC", data_transacao: "2024-05-31T03:00:00.000Z", data_pagamento: "2024-05-31T03:00:00.000Z", valor: "1534.00", status: true, conta_id: 2145231, usuario_id: 99999, transferencia_id: null, parcelamento_id: null},
+    { conta: "Conta para extrato", id: 2011779,descricao: "Movimentacao para extrato", envolvido: "FFF", observacao: null ,tipo: "DESP", data_transacao: "2024-05-31T03:00:00.000Z", data_pagamento: "2024-05-31T03:00:00.000Z", valor: "-220.00", status: true, conta_id: 2145232, usuario_id: 99999, transferencia_id: null, parcelamento_id: null},
+    { conta: "Conta para movimentacoes", id: 2011780, descricao: "desc", envolvido: "inter", observacao: null, tipo: "REC" , data_transacao: "2024-05-31T03:00:00.000Z", data_pagamento: "2024-06-01T03:00:00.000Z", valor: "123.00", status: true, conta_id: 2145229, usuario_id: 99999, transferencia_id: null, parcelamento_id: null },
+    { conta: "Conta para movimentacoes", id: 2011780, descricao: "Descrição", envolvido: "interessado", observacao: null, tipo: "REC" , data_transacao: "2024-05-31T03:00:00.000Z", data_pagamento: "2024-06-01T03:00:00.000Z", valor: "123.00", status: true, conta_id: 2145229, usuario_id: 99999, transferencia_id: null, parcelamento_id: null }
+]
+).as('estrato_com_7_registros')
+//------------------------------------------------------------------------------------
+        //TODO - Realizar essa calidação com os 7 registros adicionados
+   /*     
         cy.get('.list-group')
         .find('li')
         .its('length')
@@ -104,7 +138,7 @@ describe('Testanado em nível funcional',()=>{
         .contains('span', 'Descrição') 
         .parent() 
         .find('small') 
-        .contains('123')
+        .contains('123')*/
     })
 
     it("Deve remover a movimentação inserida",()=>{
--- cypress/01-basico/cypress/support/buildEnv.js
+++ cypress/01-basico/cypress/support/buildEnv.js
@@ -57,6 +57,40 @@ const buildEnv = () =>{
     ]
     ).as('contas')
 
+    cy.intercept({
+        method: 'POST',
+        url: 'https://barrigarest.wcaquino.me/transacoes'
+    },{
+        conta: "Conta para saldo",
+        id: 2011776,
+        descricao: "Movimentacao 1, calculo saldo",
+        envolvido: "CCC",
+        observacao: null,
+        tipo: "REC",
+        data_transacao: "2024-05-31T03:00:00.000Z",
+        data_pagamento: "2024-05-31T03:00:00.000Z",
+        valor: "3500.00",
+        status: true,
+        conta_id: 2145231,
+        usuario_id: 50833,
+        transferencia_id: null,
+        parcelamento_id: null
+    }
+    ).as('transacoes')
+
+    /*cy.intercept({
+        method: 'POST',
+        url: 'https://barrigarest.wcaquino.me/extrato/**'
+    },[
+        { conta: "Conta para saldo", id: 2011776,descricao: "Movimentacao 1, calculo saldo", envolvido: "CCC", observacao: null ,tipo: "REC", data_transacao: "2024-05-31T03:00:00.000Z", data_pagamento: "2024-05-31T03:00:00.000Z", valor: "3500.00", status: true, conta_id: 2145231, usuario_id: 99999, transferencia_id: null, parcelamento_id: null},
+        { conta: "Conta com movimentacao", id: 2011775,descricao: "Movimentacao de conta", envolvido: "BBB", observacao: null ,tipo: "DESP", data_transacao: "2024-05-31T03:00:00.000Z", data_pagamento: "2024-05-31T03:00:00.000Z", valor: "-1500.00", status: true, conta_id: 2145230, usuario_id: 99999, transferencia_id: null, parcelamento_id: null},
+        { conta: "Conta para saldo", id: 2011777,descricao: "Movimentacao 2, calculo saldo", envolvido: "DDD", observacao: null ,tipo: "DESP", data_transacao: "2024-05-31T03:00:00.000Z", data_pagamento: "2024-05-31T03:00:00.000Z", valor: "-1000.00", status: true, conta_id: 2145231, usuario_id: 99999, transferencia_id: null, parcelamento_id: null},
+        { conta: "Conta para saldo", id: 2011778,descricao: "Movimentacao 3, calculo saldo", envolvido: "EEE", observacao: null ,tipo: "REC", data_transacao: "2024-05-31T03:00:00.000Z", data_pagamento: "2024-05-31T03:00:00.000Z", valor: "1534.00", status: true, conta_id: 2145231, usuario_id: 99999, transferencia_id: null, parcelamento_id: null},
+        { conta: "Conta para extrato", id: 2011779,descricao: "Movimentacao para extrato", envolvido: "FFF", observacao: null ,tipo: "DESP", data_transacao: "2024-05-31T03:00:00.000Z", data_pagamento: "2024-05-31T03:00:00.000Z", valor: "-220.00", status: true, conta_id: 2145232, usuario_id: 99999, transferencia_id: null, parcelamento_id: null},
+        { conta: "Conta para movimentacoes", id: 2011780, descricao: "desc", envolvido: "inter", observacao: null, tipo: "REC" , data_transacao: "2024-05-31T03:00:00.000Z", data_pagamento: "2024-06-01T03:00:00.000Z", valor: "123.00", status: true, conta_id: 2145229, usuario_id: 99999, transferencia_id: null, parcelamento_id: null }
+    ]
+    ).as('estrato_com_6_registros')*/
+
 }
 
 export default buildEnv
